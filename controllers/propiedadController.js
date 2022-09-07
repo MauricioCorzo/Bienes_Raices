34b864d6b1e7ea8af4d05e383f98f9a8b2b13cdd
MauricioCorzo <mauricio.corzo@yahoo.com>
2022-09-07 20:17:39 -0300

    Editar propiedad

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -148,10 +148,100 @@ const almacenarImagen = async (req,res) => {
     }
 }
 
+const editar = async (req,res) => {
+    const { id } = req.params;
+    const { usuario } = req;
+
+    //Validar que la propiedad exista
+    const propiedad = await Propiedad.findByPk(id);
+    if(!propiedad){
+        return res.redirect("/mis-propiedades")
+    };
+
+    //Revisar que el usuario sea el que creó la propiedad
+    if(usuario.id.toString() !== propiedad.usuarioId.toString()){
+        return res.redirect("/mis-propiedades")
+    };
+
+    // Consultar Modelo de Precio Y Categoria
+    const [categorias, precios] = await Promise.all([Categoria.findAll(), Precio.findAll()]);
+
+    res.render("propiedades/editar", {
+        pagina: `Editar Propiedad: "${propiedad.titulo}"`,
+        csrfToken: req.csrfToken(),
+        categorias: categorias,
+        precios: precios,
+        datos: propiedad
+    });
+}
+
+
+const guardarCambios = async (req,res) => {
+    const { id } = req.params;
+    const { usuario } = req;
+
+    // Validacion del post
+    let resultado = validationResult(req);
+
+    if(!resultado.isEmpty()){
+        const [categorias, precios] = await Promise.all([Categoria.findAll(), Precio.findAll()]); // Se require pq si hay error en el post del form igualmente tenemos que tener 
+        // las categorias y precios
+
+        return res.render("propiedades/editar", {
+            pagina: `Editar Propiedad`,
+            csrfToken: req.csrfToken(),
+            categorias: categorias,
+            precios: precios,
+            errores: resultado.array(),
+            datos: req.body
+
+        });
+    };
+
+    //Validar que la propiedad exista
+    const propiedad = await Propiedad.findByPk(id);
+    if(!propiedad){
+        return res.redirect("/mis-propiedades")
+    };
+
+    //Revisar que el usuario sea el que creó la propiedad
+    if(usuario.id.toString() !== propiedad.usuarioId.toString()){
+        return res.redirect("/mis-propiedades")
+    };
+
+    //Actualizar Propiedad
+    try {
+        const { titulo , descripcion, habitaciones , estacionamiento , wc , calle , lat , lng, precio, categoria} = req.body;
+
+        //Casi Igual que en crear, set es un metodo de sequelize. No le pasamos la imagen ni usuario id pa ya los tenemos
+       propiedad.set({
+            titulo: titulo,
+            descripcion: descripcion,
+            habitaciones: habitaciones,
+            estacionamiento: estacionamiento,
+            wc: wc,
+            calle: calle,
+            lat: lat,
+            lng: lng,
+            precioId: precio,
+            categoriaId: categoria,
+        })
+
+        await propiedad.save()
+
+        res.redirect("/mis-propiedades")
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export {
     admin,
     crear,
     guardar, 
     agregarImagen,
-    almacenarImagen
+    almacenarImagen,
+    editar,
+    guardarCambios
 };
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,6 +1,6 @@
 import express  from "express";
 import { body } from "express-validator" // Es la validacion en el router y no en el controlador. Aqui se usa body, en el controller se usa check
-import { admin, agregarImagen, almacenarImagen, crear, guardar } from "../controllers/propiedadController.js";
+import { admin, agregarImagen, almacenarImagen, crear, editar, guardar, guardarCambios } from "../controllers/propiedadController.js";
 import protegerRuta from "../middleware/protegerRuta.js";
 import upload from "../middleware/subirImagen.js";
 
@@ -24,6 +24,16 @@ propiedadesRoutes.post("/propiedades/crear", protegerRuta,
 propiedadesRoutes.get("/propiedades/agregar-imagen/:id", protegerRuta, agregarImagen)    
 propiedadesRoutes.post("/propiedades/agregar-imagen/:id", protegerRuta, upload.single("imagen"), almacenarImagen)    // si son + imagenes se usa single.array()
 
-
+propiedadesRoutes.get("/propiedades/editar/:id", protegerRuta, editar)
+propiedadesRoutes.post("/propiedades/editar/:id", protegerRuta,
+    body("titulo").notEmpty().withMessage("El titulo del anuncio es obligatorio"),
+    body("descripcion").notEmpty().withMessage("La descripcion no puede ir vacia"),
+    body("categoria").isNumeric().withMessage("Selecciona una categoria"),   // usamos isNumeric porque el value en crear.pug es el id de cada categoria
+    body("precio").isNumeric().withMessage("Selecciona un rango de precios"),
+    body("habitaciones").isNumeric().withMessage("Selecciona la cantidad de habitaciones"),
+    body("estacionamiento").isNumeric().withMessage("Selecciona la cantidad de estacionamientos"),
+    body("wc").isNumeric().withMessage("Selecciona la cantidad de baños"),
+    body("lat").notEmpty().withMessage("Ubica la propiedad en el mapa"),
+    guardarCambios)
 
 export default propiedadesRoutes
