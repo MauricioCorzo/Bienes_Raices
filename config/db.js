71bc8ab190250cee931ea07eebf1737fd9f56fe5
MauricioCorzo <mauricio.corzo@yahoo.com>
2022-09-14 15:52:03 -0300

    Pruebas

--- config/db.js
+++ config/db.js
@@ -3,7 +3,27 @@ import dotenv from "dotenv"
 
 dotenv.config({path: '.env'})
 
-const db =  new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+const db = process.env.NODE_ENV === "production"? new Sequelize({
+  database:DB_NAME,
+  dialect: "postgres",
+  host: DB_HOST,
+  port: "5432",
+  username: DB_USER,
+  password: DB_PASSWORD,
+  pool: {
+    max: 3,
+    min: 1,
+    idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    keepAlive: true,
+  },
+  ssl: true,
+}) : new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
     host: `${process.env.DB_HOST}`,
     port: process.env.DB_PORT,
     dialect: "postgres",
